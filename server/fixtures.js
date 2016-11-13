efe75349ccd9548528160b591d54d7ef09a7dbd2
devwaat <fomeboy@gmail.com>
2016-11-13 20:31:43 +0000

    bug fixes

--- server/fixtures.js
+++ server/fixtures.js
@@ -7,7 +7,7 @@ if (Meteor.users.find().fetch().length === 0) {
   let id = Accounts.createUser({username: 'blog-admin', email: 'admin@blog.com', password: 'blog-admin'})
   Roles.addUsersToRoles(id, ['admin'])
 
-  id = Accounts.createUser({username: 'test-user', email: 'test@blog.com', password: 'te st-user'})
+  id = Accounts.createUser({username: 'test-user', email: 'test@blog.com', password: 'test-user'})
   Roles.addUsersToRoles(id, ['other'])
   console.log('Users created!')
 }
